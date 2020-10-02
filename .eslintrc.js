3b0c617761a1e0596586f5af26d34b8f8fda66c9
Daniel Fischer <daniel@d-fischer.dev>
2020-10-03 01:25:23 +0200

    allow pascal case function names in linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,18 @@
+const baseRules = require('@d-fischer/eslint-config');
+
+const namingConvention = [
+	...baseRules.rules['@typescript-eslint/naming-convention'],
+	{
+		selector: 'function',
+		format: ['strictCamelCase', 'StrictPascalCase']
+	}
+]
+
 module.exports = {
 	extends: '@d-fischer',
 
 	rules: {
-		'@typescript-eslint/no-explicit-any': 'off'
+		'@typescript-eslint/no-explicit-any': 'off',
+		'@typescript-eslint/naming-convention': namingConvention
 	}
 };
